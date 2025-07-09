2840d80ee5c6cddf3369c1543290525175b23dfb
Hubert <hubertcen93@gmail.com>
2025-07-09 11:16:34 +0700

    NativeWind Setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,16 @@
-import { Text, View } from "react-native";
+import { FlatList, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import "./globals.css";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-5xl font-bold text-primary font-quicksand-bold">
-        Welcome to react Native Application!
-      </Text>
-    </View>
+    <SafeAreaView>
+      <FlatList
+        data={offers}
+        renderItem={({ item, index }) => {
+          return <View></View>;
+        }}
+      />
+    </SafeAreaView>
   );
 }
