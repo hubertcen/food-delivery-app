d84f71ec2883539d2b7f978695c2279d91b0a08b
Hubert <hubertcen93@gmail.com>
2025-07-09 10:50:44 +0700

    NativeWind Setup

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "./globals.css";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl font-bold text-primary font-quicksand-bold">
+        Welcome to react Native Application!
+      </Text>
+    </View>
+  );
+}
